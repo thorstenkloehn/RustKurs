feb862de689f20bf51cab24395bad70b38122f81
Your Name <thorstenkloehn@gmail.com>
2026-06-15 01:04:25 +0200

    Remove generated target files and build artifacts for Uebung_2

--- Uebung/Uebung_2/target/debug/deps/Uebung_2-03c166c30492d845.d
+++
@@ -1,5 +0,0 @@
-/home/thorsten/RustKurs/Uebung/Uebung_2/target/debug/deps/Uebung_2-03c166c30492d845.d: src/main.rs
-
-/home/thorsten/RustKurs/Uebung/Uebung_2/target/debug/deps/libUebung_2-03c166c30492d845.rmeta: src/main.rs
-
-src/main.rs:
--- Uebung/Uebung_2/target/debug/deps/Uebung_2-f0c0a8f4aeefc192.d
+++
@@ -1,5 +0,0 @@
-/home/thorsten/RustKurs/Uebung/Uebung_2/target/debug/deps/Uebung_2-f0c0a8f4aeefc192.d: src/main.rs
-
-/home/thorsten/RustKurs/Uebung/Uebung_2/target/debug/deps/libUebung_2-f0c0a8f4aeefc192.rmeta: src/main.rs
-
-src/main.rs:
